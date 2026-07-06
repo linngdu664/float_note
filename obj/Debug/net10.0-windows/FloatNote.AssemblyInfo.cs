0e058d78ad0c9f01a37ac4c2b097c412184f1f92
Linngdu664 <664774652@qq.com>
2026-07-06 11:35:02 +0800

    rebuild

--- obj/Debug/net10.0-windows/FloatNote.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/FloatNote.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FloatNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c3a14abb4aff775ba170d3449b175e5d4c2f0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1c417f60a6b6f83c811a48eacc56de0f8b03bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FloatNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FloatNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
